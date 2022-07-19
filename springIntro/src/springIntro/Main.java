e70e5b182a3b48e65e1b6b05391188bd841889c2
Atakan Bayrak <74923491+atakanbayrak@users.noreply.github.com>
2022-07-19 22:14:47 +0300

    Add files via upload

---
+++ springIntro/src/springIntro/Main.java
@@ -0,0 +1,20 @@
+package springIntro;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ICustomerService customerService = context.getBean("service",ICustomerService.class);
+		
+		
+		customerService.add();
+	}
+	
+	//IoC - Inversion of Control <--- Birbirinin alternatifi olan iþleri ayrý classlarda yönetiyoruz.
+	//Dependency Injection <--- Design Pattern
+	//SOLID 
+}
